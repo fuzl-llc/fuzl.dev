8dfde95a7bf482a465f364165925a9124b26ab9b
Brent <gh.bc@fuzl.com>
2021-09-15 22:56:36 -0400

    Added YourNameIs projects.

---
+++ YourNameIs/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var chars = name.ToCharArray();
+            Array.Reverse(chars);
+            var reversedName = String.Join("", chars);
+            reversedName = reversedName.Substring(0, 1).ToUpper() + reversedName.Substring(1).ToLower();
+            Console.WriteLine($"That's a nice name, but I'm going to call you {reversedName}.");
+            Console.WriteLine($"What's up {reversedName}?!?");
+        }
+    }
+}
---
+++ YourNameIs/Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+
+        // Your code here.
+
+        Console.WriteLine($"Hi {name}!");
+    }
+}
