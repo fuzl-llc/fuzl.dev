9e50edba5aeb2387f0726231b1e5920a88a8b990
Brent <gh.bc@fuzl.com>
2021-10-07 15:23:28 -0400

    Minor refactor

--- YourNameIs/Demo/Program.cs
+++ YourNameIs/Demo/Program.cs
@@ -10,7 +10,7 @@ namespace Demo
             var name = Console.ReadLine();
             var chars = name.ToCharArray();
             Array.Reverse(chars);
-            var reversedName = String.Join("", chars);
+            var reversedName = String.Concat(chars);
             reversedName = reversedName.Substring(0, 1).ToUpper() + reversedName.Substring(1).ToLower();
             Console.WriteLine($"That's a nice name, but I'm going to call you {reversedName}.");
             Console.WriteLine($"What's up {reversedName}?!?");
