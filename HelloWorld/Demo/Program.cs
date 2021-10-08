a256b9b0b080f13556b431f5146b35b2ffd2bf28
Brent <gh.bc@fuzl.com>
2021-10-08 14:52:14 -0400

    Comments.

--- HelloWorld/Demo/Program.cs
+++ HelloWorld/Demo/Program.cs
@@ -5,3 +5,38 @@
         System.Console.WriteLine("Hello World!");
     }
 }
+
+
+// TODO: put in Exercise file:
+
+/*
+
+The /* characters denote the beginning of a "comment." Comments are ignored by the compiler and are 
+primarily for the developer to document their code. We'll use comments to describe the code we want 
+to write.
+
+First we have to create a class. We do that by typing the following text. Be sure to do this after 
+the end of this comment block which ends with the * and / character. We can't actually write them 
+together or we'd end this comment and we don't want to do that yet... So, here is what you need to 
+type, down at the bottom of this file:
+
+class Program
+{
+}
+
+Then, inside the curly brackets, we have to create a function (or "method") to be the entry point
+to the application:
+
+static void Main()
+{
+}
+
+Finally, we have to write the code we want to run when the program runs. We want to write "Hello 
+World!" to the console, so we need the following code inside the Main method.
+
+System.Console.WriteLine("Hello World!");
+
+That's it. Once you're done with the code, press Ctrl+S to save and then F5 to run it. Good luck!
+
+*/
+
